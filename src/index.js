45c89fb74519e170f4c90c1e0062a0597855312b
hituzi-exe <sparychory@gmail.com>
2018-11-08 10:14:09 +0900

    modify Store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import CalculatorContainer from './containers/CalculatorContainer'
-
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <CalculatorContainer />,
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import CalculatorContainer from './containers/CalculatorContainer';
+import reducer from './reducers';
+
+const store = createStore(reducer);
+
+render(
+    <Provider store={store}>
+        <CalculatorContainer />
+    </Provider>,
     document.getElementById('root')
 );
 
