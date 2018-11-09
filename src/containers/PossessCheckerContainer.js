50035d5b48906df480bfda313c9d23ba437e30c7
hituzi-exe <sparychory@gmail.com>
2018-11-09 09:48:51 +0900

    modify container usesr list

--- src/containers/PossessCheckerContainer.js
+++ src/containers/PossessCheckerContainer.js
@@ -8,8 +8,18 @@ import PartsElm from '../components/PartsElm';
 class PossessCheckerContainer extends Component {
     render() {
         const { possessChecker, actions } = this.props;
+        const partsList = [
+            {"idx": 1, "name": "M90"},
+            {"idx": 2, "name": "M91"}
+        ];
+
         return (
+            
             <div>
+                {partsList.map(d => { 
+                    return <li>{d.name}</li>
+                })}
+                
                 <PartsElm
                     name={possessChecker.name}
                     result={possessChecker.resultValue}
