f3d459e62c50c998b87cd09d73596331c5f416d0
hituzi-exe <sparychory@gmail.com>
2018-11-09 15:06:07 +0900

    modify PartsElm

--- src/components/PartsElm.js
+++ src/components/PartsElm.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-const PartsElm = ({ parts, onClick }) => (
-    <div onClick={onClick} className={"partsElm"}>
-        <span>WEAPON</span>
-        <div>
-            <img src="" alt="" /><span>MAIN</span>
+const PartsElm = ({ parts, onClick }) => {
+    const className = "partsElm partsElm__count_" + parts.count;
+
+    return (
+        <div onClick={onClick} className={className}>
+            <span>WEAPON</span>
+            <PartsCategory parts={parts} />
+            <PartsImage parts={parts} />
+            <PartsName parts={parts} />
+            <div><span>{parts.count}</span></div>
         </div>
-        <div>
-            <img src="" alt="" />
-        </div>
-        <div><span>{parts.name}</span></div>
-        <div><span>{parts.count}</span></div>
-    </div>
+    )
+};
+
+const PartsCategory = ({ parts }) => (
+    <div><img src="" alt="" /><span>{parts.category}</span></div>
+);
+
+const PartsImage = ({ parts }) => (
+    <div><img src={parts.imgSrc} alt="" /></div>
+);
+
+const PartsName = ({ parts }) => (
+    <div><span>{parts.name}</span></div>
 );
 
 PartsElm.propTypes = {
