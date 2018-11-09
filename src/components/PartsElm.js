98491b9de7aa4bb789c3c97e9f03a96b38606e35
hituzi-exe <sparychory@gmail.com>
2018-11-09 17:25:31 +0900

    modify show image sample code

--- src/components/PartsElm.js
+++ src/components/PartsElm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import logo from '../images/logo.svg';
 
 const PartsElm = ({ parts, onClick }) => {
     const className = "partsElm partsElm__count_" + parts.count;
@@ -15,9 +16,27 @@ const PartsElm = ({ parts, onClick }) => {
     )
 };
 
-const PartsCategory = ({ parts }) => (
-    <div><img src="" alt="" /><span>{parts.category}</span></div>
-);
+//TODO 兵装アイコン読込に変更すること
+const PartsCategory = ({ parts }) => {
+    const imgSrc = category => {
+        switch (category) {
+            case "MAIN":
+                return logo;
+            case 1:
+                return logo;
+            case 2:
+                return "";
+            case 3:
+                return "";
+            default:
+                return "";
+        }
+    };
+
+    return (
+        <div><img src={imgSrc(parts.category)} alt="" /><span>{parts.category}</span></div>
+    )
+};
 
 const PartsImage = ({ parts }) => (
     <div><img src={parts.imgSrc} alt="" /></div>
