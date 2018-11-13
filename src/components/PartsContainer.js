2f88696e8042c53ccd6e08ebe72425d62c9d5a08
hituzi-exe <sparychory@gmail.com>
2018-11-13 17:10:14 +0900

    modify NavLink

--- src/components/PartsContainer.js
+++ src/components/PartsContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PartsElm from './PartsElm';
+// import { NavLink } from 'react-router-dom'
 
 const PartsContainer = ({ partsList, actions }) => {
     return (
@@ -10,9 +11,27 @@ const PartsContainer = ({ partsList, actions }) => {
                     parts={parts}
                     onClick={() => actions.onPartsClick(parts.idx)} />
             })}
+            {/* <div>
+                Show:{' '}
+                <FilterLink filter="SHOW_ALL">All</FilterLink>{', '}
+                <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>{', '}
+                <FilterLink filter="SHOW_COMPLETED">Active</FilterLink>{', '}
+            </div> */}
         </div>
     )
 };
+
+// const FilterLink = ({ filter, children }) => (
+//     <NavLink
+//       to={filter === 'SHOW_ALL' ? '/' : `/${ filter }`}
+//       activeStyle={{
+//         textDecoration: 'none',
+//         color: 'black'
+//       }}
+//     >
+//       {children}
+//     </NavLink>
+// )  
 
 export default PartsContainer;
 
