803d7e7bbe3d68713a34da3579350227068685fa
hituzi-exe <sparychory@gmail.com>
2018-11-14 16:39:23 +0900

    remove test code

--- src/components/PartsContainer.js
+++ src/components/PartsContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PartsElm from './PartsElm';
-// import { NavLink } from 'react-router-dom'
 
 const PartsContainer = ({ partsList, actions }) => {
     return (
@@ -11,27 +10,10 @@ const PartsContainer = ({ partsList, actions }) => {
                     parts={parts}
                     onClick={() => actions.onPartsClick(parts.idx)} />
             })}
-            {/* <div>
-                Show:{' '}
-                <FilterLink filter="SHOW_ALL">All</FilterLink>{', '}
-                <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>{', '}
-                <FilterLink filter="SHOW_COMPLETED">Active</FilterLink>{', '}
-            </div> */}
         </div>
     )
 };
 
-// const FilterLink = ({ filter, children }) => (
-//     <NavLink
-//       to={filter === 'SHOW_ALL' ? '/' : `/${ filter }`}
-//       activeStyle={{
-//         textDecoration: 'none',
-//         color: 'black'
-//       }}
-//     >
-//       {children}
-//     </NavLink>
-// )  
 
 export default PartsContainer;
 
