ddf834d6a3b79b2dc31a6444de78533878b3f6b3
hituzi-exe <sparychory@gmail.com>
2018-11-15 14:42:54 +0900

    clean extract components PossessChecker

--- src/containers/PossessCheckerContainer.js
+++ src/containers/PossessCheckerContainer.js
@@ -3,39 +3,25 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import * as actions from '../actions';
-import Menu from '../components/Menu';
-import PartsHeader from '../components/PartsHeader';
-import PartsContainer from '../components/PartsContainer';
-
-//TODO 動作確認用
-import PossessURL from '../components/PossessURL';
+import PossessChecker from '../components/PossessChecker';
 
 class PossessCheckerContainer extends Component {
     render() {
-        const { possessChecker, param, actions } = this.props;
-        const { items, title } = possessChecker.menuList.find(m => m.isSelected);
+        const { possessChecker, match, actions } = this.props;
+        const param = match.params.param;
         
         //http://localhost:3000/hogehoge でparamに"hogehoge"がはいる
-        //console.debug(param);
+        console.debug(param);
         
         return (
-            <div className="PossessCheckerWrapper">
-                <div className="PossessChecker">
-                    <Menu menuList={possessChecker.menuList} actions={actions} />
-                    <div className="content">
-                        <PartsHeader title={title} partsList={items} />
-                        <PartsContainer partsList={items} actions={actions}/>
-                    </div>
-                </div>
-                <PossessURL menuList={possessChecker.menuList} />
-            </div>
+            <PossessChecker possessChecker={possessChecker} actions={actions} />
         );
     }
 }
 
 const mapState = (state, ownProps) => ({
     possessChecker: state.possessChecker,
-    param: ownProps.match.params.param,
+    match: ownProps.match,
 });
 
 const mapDispach = (dispach) => ({
