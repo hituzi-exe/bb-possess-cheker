71a0e86745754ca6a2abb38a3bf65461f5166333
hituzi-exe <sparychory@gmail.com>
2018-11-20 14:54:02 +0900

    modify initialAppStateByParam exception

--- src/reducers/possessChecker.js
+++ src/reducers/possessChecker.js
@@ -18,16 +18,20 @@ export const initialAppStateByParam = (param) => {
         return;
     }
 
-    const { weaponArray, partsArray } = decodeParam(param);
+    try {
+        const { weaponArray, partsArray } = decodeParam(param);
 
-    const weaponItems = initialAppState.menuList.find(m => m.menuType === menuTypes.MENU_WEAPON).items;
-    for (let parts of weaponItems) {
-        parts.count = weaponArray.find(p => p.idx === parts.idx).count;
-    }
-    
-    const partsItems = initialAppState.menuList.find(m => m.menuType === menuTypes.MENU_PARTS).items;
-    for (let parts of partsItems) {
-        parts.count = partsArray.find(p => p.idx === parts.idx).count;
+        const partsItems = initialAppState.menuList.find(m => m.menuType === menuTypes.MENU_PARTS).items;
+        for (let parts of partsItems) {
+            parts.count = partsArray.find(p => p.idx === parts.idx).count;
+        }
+        
+        const weaponItems = initialAppState.menuList.find(m => m.menuType === menuTypes.MENU_WEAPON).items;
+        for (let parts of weaponItems) {
+            parts.count = weaponArray.find(p => p.idx === parts.idx).count;
+        }
+    } catch (ex) { 
+        return;
     }
 }
 
